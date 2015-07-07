048d4b7c8c6554453e19794a4864ae7e0b9e2ea9
pwood1284 <pwood1284@gmail.com>
2015-07-07 07:20:39 -0400

    added business and employee controllers with login and registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,27 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def hello
-    render text: "Hi Sr. Wood, this is out new app website. I am commited to get this app up and running"
+  def current_business_user
+    token = request.headers['Access-Token']
+    token && BusinessUser.find_by(access_token: token)
+  end
+
+  def authenticate_business_user_with_token!
+    unless current_business_user
+      render json: { message: "Business Access Token Not Found." },
+        status: :unauthenticated
+    end
+  end
+
+  def current_employee_user
+    token2 = request.headers['Access-Token2']
+    token2 && EmployeeUser.find_by(access_token2: token2)
+  end
+
+  def authenticate_employee_user_with_token!
+    unless current_employee_user
+      render json: { message: "Employee Access Token Not Found." },
+        status: :unauthenticated
+    end
   end
 end
