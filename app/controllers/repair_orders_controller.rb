b6fa7cb932aa3c0f21313eaa843ab1c0cff7e930
Juan M. Ordaz Velazquez <juanordaz_2011@icloud.com>
2015-07-08 14:12:15 -0400

    Fixed a repair orders controller

--- app/controllers/repair_orders_controller.rb
+++ app/controllers/repair_orders_controller.rb
@@ -7,28 +7,27 @@ class RepairOrdersController < ApplicationsController
   end
 
   def repair_order_create
-    @repair_order - RepairOrder.new(repair_order_number: params[:repair_order_number])
-      if @repair_order.save
-        render json: { repair_order: @repair_order.as_json },
-          status: :created
-      else
-        render json: { errors: @repair_order.errors.full_messages },
-          status: :unprocessable_entity
-      end
+    @repair_order = RepairOrder.new(repair_order_number: params[:repair_order_number])
+    if @repair_order.save
+    render json: { repair_order: @repair_order.as_json },
+      status: :created
+    else
+      render json: { errors: @repair_order.errors.full_messages },
+      status: :unprocessable_entity
     end
   end
 
   def repair_order_show
-    @repair_order = RepairOrder.find(params[:id])
+    # @repair_order = RepairOrder.find(params[:id])
 
-    if @repair_order.save
-     # render json "show.json.jbuilder", status: :ok
-     render json: { repair_order: @repair_order.as_json(include: { client: { only: [:client_first_name, :client_last_name] } } ),
-         status: :ok
-     else
-       render json: { errors: @post.errors.full_messages },
-         status: :not_found
-     end
+    # if @repair_order.save
+    #  # render json "show.json.jbuilder", status: :ok
+    #   render json: {repair_order: @repair_order.as_json( include: { client: { only: [:client_first_name, :client_last_name]}}),
+    #   status: :ok
+    # else
+    #   render json: { errors: @repair_order.errors.full_messages},
+    #   status: :not_found
+    # end
   end
 
   def repair_order_update
