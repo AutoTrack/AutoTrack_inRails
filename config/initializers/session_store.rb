57697eb33eea4ed3cfda60009e7d546e61744286
pwood1284 <pwood1284@gmail.com>
2015-07-11 09:32:57 -0400

    Adjusting seesions store in initializers to handle multiple token auth

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_AutoTrack_session'
+Rails.application.config.session_store :cookie_store, key: '_AutoTrack_session', :domain => "https://rocky-hollows-1826.herokuapp.com"
