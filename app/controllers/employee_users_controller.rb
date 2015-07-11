9dfa7db42ed438a16321db544ff16844cb32c940
pwood1284 <pwood1284@gmail.com>
2015-07-10 22:18:51 -0400

    test

--- app/controllers/employee_users_controller.rb
+++ app/controllers/employee_users_controller.rb
@@ -1,7 +1,7 @@
 class EmployeeUsersController < ApplicationController
-  skip_authorization_check
-  before_action :authenticate_business_user_with_token!
 
+  before_action :authenticate_business_user_with_token!
+  skip_authorization_check
   def index
     @employee_users = EmployeeUser.all
     render json: { employee_user: @employee_users.as_json(only: [:id, :employee_first_name,
@@ -13,14 +13,15 @@ class EmployeeUsersController < ApplicationController
 
   def super_employee_register
      passhash = Digest::SHA1.hexdigest(params[:employee_password])
+     binding.pry
      @employee_user = current_business_user.employee_users.new(employee_email: params[:employee_email],
                                        employee_pin: params[:employee_pin],
                                        employee_password: passhash,
                                        employee_first_name: params[:employee_first_name],
                                        employee_last_name: params[:employee_last_name],
-                                       employee_access_rights: params[:employee_access_rights],
                                        employee_number: params[:employee_number],
                                        role: params[:role])
+
      if @employee_user.save
 
        render json: { employee_user: @employee_user.as_json },
@@ -39,7 +40,6 @@ class EmployeeUsersController < ApplicationController
                                        employee_password: passhash,
                                        employee_first_name: params[:employee_first_name],
                                        employee_last_name: params[:employee_last_name],
-                                       employee_access_rights: params[:employee_access_rights],
                                        employee_number: params[:employee_number],
                                        role: params[:role])
      if @employee_user.save
