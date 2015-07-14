5c899de555cb49ada0aac6eb6827719ec2ea3b96
Juan M. Ordaz Velazquez <juanordaz_2011@icloud.com>
2015-07-14 13:15:44 -0400

    fixed bug in mailer

--- app/mailers/business_user_mailer.rb
+++ app/mailers/business_user_mailer.rb
@@ -1,12 +1,12 @@
-# class BusinessUserMailer < ApplicationMailer
-#     default from: 'no-reply@autotrak.com'
-#     #default to: 'juanordaz_2011@icloud'
+class BusinessUserMailer < ApplicationMailer
+    default from: 'no-reply@autotrak.com'
+    #default to: 'juanordaz_2011@icloud'
 
-#     def new_business_user(employee_user)
-#         @employee_user = employee_user
-#         mail(to: employee_user.employee_user_email,
-#                 subject: 'Welcome to AutoTrak!')
-#     end
+    def new_business_user(employee_user)
+        @employee_user = employee_user
+        mail(to: employee_user.employee_email,
+                subject: 'Welcome to AutoTrak!')
+    end
 
 
-# end
+end
