69b8a2cd69a7faf67fa80f18c131993b8d0d77c3
pwood1284 <pwood1284@gmail.com>
2015-07-15 07:45:05 -0400

    updated models

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,6 +2,7 @@ class Invoice < ActiveRecord::Base
   belongs_to :business_user
   belongs_to :repair_order
   belongs_to :client
+  belongs_to :vehicle
 
 
 
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,6 +1,7 @@
 class Vehicle < ActiveRecord::Base
   belongs_to :client
   has_many :repair_orders
+  has_many :invoices
 
 
 end
