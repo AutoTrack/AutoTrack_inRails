9126244de9cf890713989fc88510b044664617d3
pwood1284 <pwood1284@gmail.com>
2015-07-16 10:26:50 -0400

    updated routes for inventory

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150716121442) do
+ActiveRecord::Schema.define(version: 20150716140546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -139,6 +139,8 @@ ActiveRecord::Schema.define(version: 20150716121442) do
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
     t.integer  "invoice_id"
+    t.integer  "business_user_id"
+    t.string   "vehicle_sub_model"
   end
 
 end
