47634d680eeefeb8c9b487870a0590709981a945
pwood1284 <pwood1284@gmail.com>
2015-07-17 08:19:48 -0400

    Updated associations and removed business user id that was un needed in employeeusersrepairorders join table

--- app/models/employee_users_repair_order.rb
+++ app/models/employee_users_repair_order.rb
@@ -1,6 +1,7 @@
 class EmployeeUsersRepairOrder < ActiveRecord::Base
-  belongs_to :repair_order
   belongs_to :business
-  has_many :employee_users
+
+  belongs_to :repair_order
+  belongs_to :employee_user
 
 end
