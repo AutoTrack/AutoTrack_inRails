863b5feac96fdb5560b55fe4d1784af050eb1f8c
pwood1284 <pwood1284@gmail.com>
2015-07-18 16:28:39 -0400

    more json for business ros

--- app/controllers/repair_orders_controller.rb
+++ app/controllers/repair_orders_controller.rb
@@ -14,8 +14,10 @@ class RepairOrdersController < ApplicationController
 
    if @business_repair_orders
     # render json: { repair_orders: @business_repair_orders.as_json(include:=>{ [:client, :vehicle])} },
-    render json: {business_repair_orders: @business_repair_orders.as_json},
+    render json: {repair_order: @business_repair_orders.as_json(include: :client),
+                  vehicle: @business_repair_orders.as_json(include: :vehicle) },
     status: :ok
+
    else
      render json: { business_repair_orders: @business_repair_orders.error.full_messages },
      status: :unprocessable_entity
