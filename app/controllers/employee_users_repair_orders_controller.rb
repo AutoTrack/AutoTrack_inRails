e2f4184101c600bdd9ac3d8bd872efd86f3528c0
pwood1284 <pwood1284@gmail.com>
2015-07-18 14:30:08 -0400

    fixed bug

--- app/controllers/employee_users_repair_orders_controller.rb
+++ app/controllers/employee_users_repair_orders_controller.rb
@@ -3,12 +3,19 @@ class EmployeeUsersRepairOrdersController < ApplicationController
   before_action :authenticate_business_user_with_token!
 # This will attach employee to RO after the RO has been created.
   def repair_order_employees_create
+    @business_id = current_business_user.id
+    @client_id = current_client.id
+    @vehicle_id = current_vehicle.id
+    @repair_order = current_repair_order.employee_users_repair_orders.new(
+                 business_user_id: @business_id,
+                 client_id: @client_id,
+                 vehicle_id: @vehicle_id,
+                 employee_user_id: params[:employee_user_id])
 
-    @repair_order = current_business_user.repair_orders.find(params[:id])
 
-    @employee_user = current_business_user.employee_users.find(params[:employee_user_id])
-
-    @repair_order.employee_users << @employee_user
+    # @employee_user = current_business_user.employee_users.find(params[:employee_user_id])
+    #
+    # @repair_order.employee_users << @employee_user
 
       if @repair_order.save
         render json: { repair_order: @repair_order.as_json,
