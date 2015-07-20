9946623edb8f7480abc52987af2c834af316b5eb
pwood1284 <pwood1284@gmail.com>
2015-07-20 08:59:55 -0400

    faded out invoices

--- app/mailers/business_user_mailer.rb
+++ app/mailers/business_user_mailer.rb
@@ -1,13 +1,13 @@
-class BusinessUserMailer < ApplicationMailer
-    default from: 'no-reply@autotrak.com'
-    #default to: 'juanordaz_2011@icloud'
-
-    def new_business_user(employee_user)
-        @employee_user = employee_user
-        mail(to: employee_user.employee_email,
-                subject: 'Welcome to AutoTrak!')
-    end
-
-
-
-end
+# class BusinessUserMailer < ApplicationMailer
+#     default from: 'no-reply@autotrak.com'
+#     #default to: 'juanordaz_2011@icloud'
+#
+#     def new_business_user(employee_user)
+#         @employee_user = employee_user
+#         mail(to: employee_user.employee_email,
+#                 subject: 'Welcome to AutoTrak!')
+#     end
+#
+#
+#
+# end
