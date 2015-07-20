6773f9f63a65932d2dd720bc896e620db535691d
pwood1284 <pwood1284@gmail.com>
2015-07-20 07:51:33 -0400

    db migrate remove business user id

--- db/migrate/20150717121811_remove_business_user_id_from_employee_users_repair_orders.rb
+++ db/migrate/20150717121811_remove_business_user_id_from_employee_users_repair_orders.rb
@@ -1,4 +1,5 @@
 class RemoveBusinessUserIdFromEmployeeUsersRepairOrders < ActiveRecord::Migration
   def change
+    remove_column :employee_users_repair_orders, :business_user_id, :integer
   end
 end
