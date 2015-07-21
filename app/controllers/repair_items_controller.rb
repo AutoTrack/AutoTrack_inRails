8f76f40f160ffb7ed386f94b4dbb71c9aa846e1a
pwood1284 <pwood1284@gmail.com>
2015-07-20 20:36:52 -0400

    bug

--- app/controllers/repair_items_controller.rb
+++ app/controllers/repair_items_controller.rb
@@ -34,7 +34,8 @@ class RepairItemsController < ApplicationController
                                                                        :inventory_item]) },
         status: :ok
     else
-      flash[:error] = 'Please select a quantity'
+      render json: { errors: @add_item_quantity.errors.full_messages },
+      status: :unprocessable_entity
     end
   end
 
@@ -54,50 +55,48 @@ class RepairItemsController < ApplicationController
       status: :ok
   end
 
-  # def show_repair_items
-  #   @repair_order_number = current_repair_order.repair_order_number
-  #   @repair_items = current_repair_order.repair_items.all
-  #   @repair_items.each do |ri|
-  #   if ri.checked_out==false
-  #     render json: { repair_items: @repair_items.as_json(include: [:repair_order,
-  #                                                                 :inventory_item]) },
-  #       status: :ok
-  #   else
-  #     render json: { message: "There are currently no repair items attached to Repair Order #{@repair_order_number}" },
-  #       status: :ok
-  #   end
-  # end
-  #
-  # def checkout_repair_items
-  #   @repair_order_number = current_repair_order.repair_order_number
-  #   @repair_order = current_repair_order.repair_items.each do |ri|
-  #     if ri.checked_out == false
-  #       new_quantity = ri.inventory_item.inventory_count - ri.repair_item_quantity
-  #
-  #       ri.inventory_item.update(inventory_count: new_quantity)
-  #       ri.repair_item.update(checked_out: true)
-  #
-  #       render json: { message: "Repair items have been successfully checked out for Repair Order #{@repair_order_number}" },
-  #         status: :ok
-  #     else
-  #       render json: { message: "There are no repair items to checkout for Repair Order #{@repair_order_number}" },
-  #         status: :ok
-  #     end
-  #
-  # end
-  #
-  # def show_checkout_history
-  #   @repair_order_number = current_repair_order.repair_order_number
-  #   @repair_items = current_repair_order.repair_items.each do |ri|
-  #     if ri.checked_out == true
-  #       render json: { repair_items_history: @repair_items.as_json(include: [:repair_order,
-  #                                                                   :inventory_item]) },
-  #         status: :ok
-  #     else
-  #       render json: { message: "There is no history of repair items attached to Repair Order #{@repair_order_number}" },
-  #         status: :ok
-  #     end
-  #
-  # end
+  def show_repair_items
+    @repair_order_number = current_repair_order.repair_order_number
+    @repair_items = current_repair_order.repair_items.all
+    @repair_items.each do |ri|
+    if ri.checked_out==false
+      render json: { repair_items: @repair_items.as_json(include: [:repair_order,
+                                                                  :inventory_item]) },
+        status: :ok
+    else
+      render json: { message: "There are currently no repair items attached to Repair Order #{@repair_order_number}" },
+        status: :ok
+    end
+  end
 
+  def checkout_repair_items
+    @repair_order_number = current_repair_order.repair_order_number
+    @repair_order = current_repair_order.repair_items.each do |ri|
+      if ri.checked_out == false
+        new_quantity = ri.inventory_item.inventory_count - ri.repair_item_quantity
+
+        ri.inventory_item.update(inventory_count: new_quantity)
+        ri.repair_item.update(checked_out: true)
+
+        render json: { message: "Repair items have been successfully checked out for Repair Order #{@repair_order_number}" },
+          status: :ok
+      else
+        render json: { message: "There are no repair items to checkout for Repair Order #{@repair_order_number}" },
+          status: :ok
+      end
+
+  end
+
+  def show_checkout_history
+    @repair_order_number = current_repair_order.repair_order_number
+    @repair_items = current_repair_order.repair_items.each do |ri|
+      if ri.checked_out == true
+        render json: { repair_items_history: @repair_items.as_json(include: [:repair_order,
+                                                                    :inventory_item]) },
+          status: :ok
+      else
+        render json: { message: "There is no history of repair items attached to Repair Order #{@repair_order_number}" },
+          status: :ok
+      end
+  end
 end
