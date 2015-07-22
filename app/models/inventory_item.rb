3829ef2415a0fb4b2d9fe592c9ae419315ec3dfa
pwood1284 <pwood1284@gmail.com>
2015-07-21 21:04:42 -0400

    validations fix

--- app/models/inventory_item.rb
+++ app/models/inventory_item.rb
@@ -6,6 +6,10 @@ class InventoryItem < ActiveRecord::Base
 
   validates_presence_of :inventory_count, :part_number, :part_name, :business_part_number,
     :category, :inventory_item_location, :inventory_item_supplier, :reorder_alert,
-    :order_to_quantity, :inventory_item_billable, :inventory_item_taxable,
-    :inventory_item_cost, :inventory_item_markup, :inventory_count, :tool
+    :order_to_quantity, :inventory_item_cost, :inventory_item_markup, :inventory_count
+
+    validates_inclusion_of :inventory_item_billable, :in => [true, false].
+    validates_inclusion_of :inventory_item_taxable, :in => [true, false].
+    validates_inclusion_of :tool, :in => [true, false]
+
 end
